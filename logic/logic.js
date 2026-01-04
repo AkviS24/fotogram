0f9a145480a5b28dff55de2546d85e0cd5727b90
Akvis24 <akvis2022@gmail.com>
2026-01-04 15:57:32 +0100

    logic updated

--- logic/logic.js
+++ logic/logic.js
@@ -1,3 +1,16 @@
 console.log("run");
 
 const imageRef = document.getElementById("image-container");
+const images = [
+    "Alaska.jpg",
+    "Anime.jpg",
+    "Atmosphere.jpg",
+    "Blue-tit.jpg",
+    "Hurricane.jpg",
+    "Lake.jpg",
+    "Moorente.jpg",
+    "Sea.jpg",
+    "Snow-bunting.jpg",
+    "Snow-leopard.jpg",
+    "Travel.jpg"
+];
